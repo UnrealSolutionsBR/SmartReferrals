275aaa5c095643d7f04facc002f3ea711c44e370
UnrealSolutionsBR <hola@unrealsolutions.com.br>
2024-11-23 14:52:55 -0400

    Added Elementor styling options for Referral URL widget input and button

--- includes/widgets/class-sr-referral-url-widget.php
+++ includes/widgets/class-sr-referral-url-widget.php
@@ -6,6 +6,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 use Elementor\Widget_Base;
 use Elementor\Controls_Manager;
+use Elementor\Group_Control_Typography;
+use Elementor\Group_Control_Border;
+use Elementor\Group_Control_Box_Shadow;
 
 class SR_Referral_URL_Widget extends Widget_Base {
 
@@ -26,7 +29,145 @@ class SR_Referral_URL_Widget extends Widget_Base {
     }
 
     protected function register_controls() {
-        // Here you can add controls for customizing the widget if needed
+        // Input Styles
+        $this->start_controls_section(
+            'input_styles',
+            [
+                'label' => __( 'Input Styles', 'smart-referrals' ),
+                'tab'   => Controls_Manager::TAB_STYLE,
+            ]
+        );
+
+        $this->add_responsive_control(
+            'input_width',
+            [
+                'label'      => __( 'Width', 'smart-referrals' ),
+                'type'       => Controls_Manager::SLIDER,
+                'size_units' => [ '%', 'px' ],
+                'range'      => [
+                    '%' => [ 'min' => 10, 'max' => 100 ],
+                    'px' => [ 'min' => 50, 'max' => 500 ],
+                ],
+                'selectors'  => [
+                    '{{WRAPPER}} #sr-referral-link' => 'width: {{SIZE}}{{UNIT}};',
+                ],
+            ]
+        );
+
+        $this->add_responsive_control(
+            'input_height',
+            [
+                'label'      => __( 'Height', 'smart-referrals' ),
+                'type'       => Controls_Manager::SLIDER,
+                'size_units' => [ 'px' ],
+                'range'      => [
+                    'px' => [ 'min' => 20, 'max' => 100 ],
+                ],
+                'selectors'  => [
+                    '{{WRAPPER}} #sr-referral-link' => 'height: {{SIZE}}{{UNIT}};',
+                ],
+            ]
+        );
+
+        $this->add_control(
+            'input_align',
+            [
+                'label'     => __( 'Align', 'smart-referrals' ),
+                'type'      => Controls_Manager::CHOOSE,
+                'options'   => [
+                    'left'   => [
+                        'title' => __( 'Left', 'smart-referrals' ),
+                        'icon'  => 'eicon-text-align-left',
+                    ],
+                    'center' => [
+                        'title' => __( 'Center', 'smart-referrals' ),
+                        'icon'  => 'eicon-text-align-center',
+                    ],
+                    'right'  => [
+                        'title' => __( 'Right', 'smart-referrals' ),
+                        'icon'  => 'eicon-text-align-right',
+                    ],
+                ],
+                'selectors' => [
+                    '{{WRAPPER}} .sr-referral-copylink' => 'text-align: {{VALUE}};',
+                ],
+            ]
+        );
+
+        $this->add_responsive_control(
+            'input_padding',
+            [
+                'label'      => __( 'Padding', 'smart-referrals' ),
+                'type'       => Controls_Manager::DIMENSIONS,
+                'size_units' => [ 'px', '%' ],
+                'selectors'  => [
+                    '{{WRAPPER}} #sr-referral-link' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+                ],
+            ]
+        );
+
+        $this->end_controls_section();
+
+        // Button Styles
+        $this->start_controls_section(
+            'button_styles',
+            [
+                'label' => __( 'Button Styles', 'smart-referrals' ),
+                'tab'   => Controls_Manager::TAB_STYLE,
+            ]
+        );
+
+        $this->add_control(
+            'button_icon_size',
+            [
+                'label'      => __( 'Icon Size', 'smart-referrals' ),
+                'type'       => Controls_Manager::SLIDER,
+                'size_units' => [ 'px' ],
+                'range'      => [
+                    'px' => [ 'min' => 10, 'max' => 100 ],
+                ],
+                'selectors'  => [
+                    '{{WRAPPER}} #sr-copy-button img' => 'width: {{SIZE}}{{UNIT}}; height: {{SIZE}}{{UNIT}};',
+                ],
+            ]
+        );
+
+        $this->add_control(
+            'button_icon_color',
+            [
+                'label'     => __( 'Icon Color', 'smart-referrals' ),
+                'type'      => Controls_Manager::COLOR,
+                'selectors' => [
+                    '{{WRAPPER}} #sr-copy-button img' => 'filter: invert(1); color: {{VALUE}};',
+                ],
+            ]
+        );
+
+        $this->add_responsive_control(
+            'button_padding',
+            [
+                'label'      => __( 'Padding', 'smart-referrals' ),
+                'type'       => Controls_Manager::DIMENSIONS,
+                'size_units' => [ 'px', '%' ],
+                'selectors'  => [
+                    '{{WRAPPER}} #sr-copy-button' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+                ],
+            ]
+        );
+
+        $this->add_responsive_control(
+            'button_margin',
+            [
+                'label'      => __( 'Margin', 'smart-referrals' ),
+                'type'       => Controls_Manager::DIMENSIONS,
+                'size_units' => [ 'px', '%' ],
+                'selectors'  => [
+                    '{{WRAPPER}} #sr-copy-button' => 'margin: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+                ],
+            ]
+        );
+
+        $this->end_controls_section();
     }
 
     protected function render() {
@@ -45,9 +186,5 @@ class SR_Referral_URL_Widget extends Widget_Base {
         } else {
             echo '<p>' . __( 'Please log in to see your referral URL.', 'smart-referrals' ) . '</p>';
         }
-    }
-
-    protected function content_template() {
-        // Optional: Add JS template if needed
     }
 }

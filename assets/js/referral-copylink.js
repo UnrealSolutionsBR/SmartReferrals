7a40a5686c25e578b58b629ff7811b49c76b1195
UnrealSolutionsBR <hola@unrealsolutions.com.br>
2024-11-24 11:53:06 -0400

    Fixed button copy functionality to support textarea in Referral URL widget

--- assets/js/referral-copylink.js
+++ assets/js/referral-copylink.js
@@ -1,31 +1,34 @@
 jQuery(document).ready(function($) {
     $('#sr-copy-button').on('click', function(e) {
         e.preventDefault();
-        var copyText = $('#sr-referral-link').val();
 
-        // Copy the text to clipboard without selecting the input
-        navigator.clipboard.writeText(copyText).then(function() {
-            // Show notification
-            if ( $('.sr-copylink-notification').length === 0 ) {
+        // Seleccionar el texto del textarea
+        var $textarea = $('.sr-referral-input');
+        $textarea.select();
+
+        // Copiar el texto al portapapeles
+        navigator.clipboard.writeText($textarea.val()).then(function() {
+            // Mostrar notificación
+            if ($('.sr-copylink-notification').length === 0) {
                 $('body').append('<div class="sr-copylink-notification">' + srCopyLink.copiedText + '<span class="sr-close-notification">×</span></div>');
             }
             var notification = $('.sr-copylink-notification');
 
-            // Adjust position 20px below the header
+            // Ajustar posición (si hay un admin bar)
             var headerHeight = $('header').outerHeight() || $('#wpadminbar').outerHeight() || 0;
             notification.css('top', (headerHeight + 40) + 'px');
             notification.addClass('show');
 
-            // Close notification on click
+            // Cerrar notificación al hacer clic en el botón de cerrar
             notification.find('.sr-close-notification').on('click', function() {
                 notification.removeClass('show');
             });
 
-            // Automatically hide after 5 seconds
+            // Ocultar automáticamente después de 5 segundos
             setTimeout(function() {
                 notification.removeClass('show');
             }, 5000);
-        }, function(err) {
+        }).catch(function(err) {
             console.error('Could not copy text: ', err);
         });
     });
